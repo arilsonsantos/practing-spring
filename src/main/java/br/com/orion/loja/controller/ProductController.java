52a984f8c6a62f57c972e989041aa0723bbb6f19
Arilson Santos <arilson@msn.com>
2019-06-22 13:09:27 -0300

    Security with user in memory

--- src/main/java/br/com/orion/loja/controller/ProductController.java
+++ src/main/java/br/com/orion/loja/controller/ProductController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.OK;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class ProductController {
         return "Hello";
     }
     
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping(path = "protected/products")
     public ResponseEntity<?> findAll() {
         return new ResponseEntity<>(productService.findAll(), OK);
