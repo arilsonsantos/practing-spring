dc261a5a378a07721f9c4e77a5b317cfd8878298
Arilson Santos <arilson@msn.com>
2019-06-22 19:05:53 -0300

    Added SessionCreationPolicy.STATELESS
    SecurityConfig

--- src/main/java/br/com/orion/loja/config/SecurityConfig.java
+++ src/main/java/br/com/orion/loja/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * SecurityConfig
@@ -23,7 +24,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
         .anyRequest()
         .authenticated()
         .and()
