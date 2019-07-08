d25c0a7ee88bcfb12537caefd5198a7996598676
Arilson Santos <arilson@msn.com>
2019-07-08 11:05:38 -0300

    Authentication using USER from DATABASE

--- src/main/java/br/com/orion/loja/config/SecurityConfig.java
+++ src/main/java/br/com/orion/loja/config/SecurityConfig.java
@@ -1,25 +1,32 @@
 package br.com.orion.loja.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.orion.loja.service.UserDetailApplicationService;
 
 /**
  * SecurityConfig
  */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+
+    @Autowired
+    private  UserDetailApplicationService userDetailApplicationService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        .withUser("joao").password("{noop}1234").roles("USUARIO")
-        .and()
-        .withUser("maria").password("{noop}1234").roles("ADMINISTRADOR");
+        auth.userDetailsService(userDetailApplicationService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -39,6 +46,11 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
         .httpBasic()
         .and()
         .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
     
 }
