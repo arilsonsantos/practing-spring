2c6bfc66d4d7dad111c6056964cba4ef2f9d539a
Arilson Santos <arilson@msn.com>
2019-07-09 10:31:07 -0300

    Suite Tests

--- src/test/java/br/com/orion/loja/repository/ProductRepositoryTest.java
+++ src/test/java/br/com/orion/loja/repository/ProductRepositoryTest.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @Slf4j
-public class TestProductRepository {
+public class ProductRepositoryTest {
 
     @Autowired
     private ProductService productService;
